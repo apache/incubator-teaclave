e8f86bdafa3e0338fb2adb83e9fc3ab2f85d79e4
RundongZhou <rundongzhou@gmail.com>
2019-10-31 19:44:11 -0700

    Create symlinks to make remapped paths accessible and avoid repeated building caused by num-traits.

--- cmake/scripts/rustc_wrapper.sh
+++ cmake/scripts/rustc_wrapper.sh
@@ -6,4 +6,4 @@ for var in "${REQUIRED_ENVS[@]}"; do
 done
 
 # Tell rustc to remap absolute src paths to make enclaves' signature more reproducible
-exec rustc "$@" --remap-path-prefix=${HOME}/.cargo=/cargo_home --remap-path-prefix=${MESATEE_PROJECT_ROOT}=/mesatee_src --remap-path-prefix=${MESATEE_BUILD_ROOT}=/mesatee_build
+exec rustc "$@" --remap-path-prefix=${HOME}/.cargo=/tmp/mesatee_symlinks/cargo_home --remap-path-prefix=${MESATEE_PROJECT_ROOT}=/tmp/mesatee_symlinks/mesatee_src --remap-path-prefix=${MESATEE_BUILD_ROOT}=/tmp/mesatee_symlinks/mesatee_build
