ec3d1a97aa6c7e0513a8d25351693ffce30bbba0
Mingshen Sun <bob@mssun.me>
2019-12-10 20:52:51 -0800

    Update build.rs of config to handle SIM_MODE

--- teaclave_config/build.rs
+++ teaclave_config/build.rs
@@ -3,6 +3,12 @@ use std::path::Path;
 use std::process::Command;
 
 fn main() {
+    let is_sim = env::var("SGX_MODE").unwrap_or_else(|_| "HW".to_string());
+    match is_sim.as_ref() {
+        "HW" => {}
+        _ => println!("cargo:rustc-cfg=sgx_sim"),
+    }
+
     let out_dir = env::var("OUT_DIR").expect("$OUT_DIR not set. Please build with cargo");
     let dest_file = Path::new(&out_dir).join("build_config.rs");
     println!("cargo:rerun-if-changed=config_gen/main.rs");
