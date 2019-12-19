f157c7831b26ba99bb7b499d1f7ff9244f419692
Mingshen Sun <bob@mssun.me>
2019-12-19 01:33:05 -0800

    make format

--- teaclave_common/protected_fs_rs/src/lib.rs
+++ teaclave_common/protected_fs_rs/src/lib.rs
@@ -17,9 +17,9 @@
 #![cfg_attr(feature = "mesalock_sgx", no_std)]
 
 #[cfg(feature = "mesalock_sgx")]
-extern crate sgx_tstd as std;
-#[cfg(feature = "mesalock_sgx")]
 extern crate sgx_trts;
+#[cfg(feature = "mesalock_sgx")]
+extern crate sgx_tstd as std;
 #[cfg(feature = "mesalock_sgx")]
 extern crate sgx_types;
 
