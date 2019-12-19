6bd83ee5a5beb4cf2bc298c1b01c724d5dfaf193
Tongxin Li <litongxin1991@gmail.com>
2019-12-19 21:33:39 +0000

    fix format

--- mesatee_services/tdfs/internal_proto/build.rs
+++ mesatee_services/tdfs/internal_proto/build.rs
@@ -37,7 +37,10 @@ fn main() {
     config.extern_path(".kms_proto", "kms_proto::proto");
     config.extern_path(".cred_proto", "authentication_proto::proto");
     config.extern_path(".data_common", "tdfs_common_proto::data_common_proto");
-    config.extern_path(".function_common", "tdfs_common_proto::function_common_proto");
+    config.extern_path(
+        ".function_common",
+        "tdfs_common_proto::function_common_proto",
+    );
     config.out_dir(output);
     config
         .compile_protos(&to_compiled_protos, includes)
