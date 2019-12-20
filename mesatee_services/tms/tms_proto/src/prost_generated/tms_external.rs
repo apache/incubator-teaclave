0e30ef0fa404a1fd8baad9e0fe6e2a92ef78da0c
Tongxin Li <litongxin1991@gmail.com>
2019-12-20 21:28:01 +0000

    fix minor error

--- mesatee_services/tms/tms_proto/src/prost_generated/tms_external.rs
+++ mesatee_services/tms/tms_proto/src/prost_generated/tms_external.rs
@@ -1,93 +1,136 @@
+/// input argument in TaskAgreement
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct InputArg {
+    /// private input or fusion input
     #[prost(bool, required, tag="1")]
     pub is_private: bool,
+    /// owners of the input: private - single owner, fusion - multiple owners
     #[prost(string, repeated, tag="2")]
     pub owners: ::std::vec::Vec<std::string::String>,
+    /// data id of the input
     #[prost(string, required, tag="3")]
     pub data_id: std::string::String,
 }
+/// output argument in TaskAgreement
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct OutputArg {
+    /// private output or fusion output
     #[prost(bool, required, tag="1")]
     pub is_private: bool,
+    /// owners of the input: private - single owner, fusion - multiple owners
     #[prost(string, repeated, tag="2")]
     pub owners: ::std::vec::Vec<std::string::String>,
+    /// data id of the output
+    /// fusion output may not have a data id before the TaskAgreement is accepted by the TMS
     #[prost(string, optional, tag="3")]
     pub data_id: ::std::option::Option<std::string::String>,
 }
+/// TaskAgreement contains all the information to invoke a task
+/// But sensitive data is represented by data id.
+/// Multiple participants can consent to the TaskAgreement offline.
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct TaskAgreement {
+    /// Task identifier
     #[prost(string, required, tag="1")]
     pub task_id: std::string::String,
+    /// Function identifier
     #[prost(string, required, tag="2")]
     pub function_id: std::string::String,
+    /// Input arguments
     #[prost(map="string, message", tag="3")]
     pub input_arguments: ::std::collections::HashMap<std::string::String, InputArg>,
+    /// Output arguments
     #[prost(map="string, message", tag="4")]
     pub output_argments: ::std::collections::HashMap<std::string::String, OutputArg>,
+    /// Participants
     #[prost(string, repeated, tag="5")]
     pub participants: ::std::vec::Vec<std::string::String>,
+    /// Signatures of the participants
     #[prost(map="string, bytes", tag="6")]
     pub signatures: ::std::collections::HashMap<std::string::String, std::vec::Vec<u8>>,
 }
+/// Request for registering a Task, in order to propose a TaskAgreement
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct RegisterTaskRequest {
+    /// Function identifier
     #[prost(string, required, tag="1")]
     pub function_id: std::string::String,
+    /// Participants
     #[prost(string, repeated, tag="2")]
     pub participants: ::std::vec::Vec<std::string::String>,
+    /// User credential
     #[prost(message, required, tag="99")]
     pub creds: authentication_proto::proto::Credential,
 }
+/// Response for registering a Task
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct RegisterTaskResponse {
+    /// initial TaskAgreement
     #[prost(message, required, tag="1")]
     pub initial_agreement: TaskAgreement,
 }
+/// Request for getting a Task
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct GetTaskRequest {
+    /// Task identifier
     #[prost(string, required, tag="1")]
     pub task_id: std::string::String,
+    /// User credential
     #[prost(message, required, tag="99")]
     pub creds: authentication_proto::proto::Credential,
 }
+/// Response for getting a Task
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct GetTaskResponse {
+    /// TaskAgreement
     #[prost(message, required, tag="1")]
     pub agreement: TaskAgreement,
+    /// Task Status
     #[prost(enumeration="TaskStatus", required, tag="2")]
     pub statuts: i32,
 }
+/// Request for invoking a Task
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct InvokeTaskRequest {
+    /// A complete TaskAgreement: required inputs and outputs are provided, all the participants approved the task.
     #[prost(message, required, tag="1")]
     pub complete_agreement: TaskAgreement,
+    /// User credential
     #[prost(message, required, tag="99")]
     pub creds: authentication_proto::proto::Credential,
 }
+/// Response for invoking a Task
+/// The invocation is asynchronous. Participants need to use GetTaskRequest to query the task status.
+/// Participants need to use the (logical and physical) data information they provided to access the output data.
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct InvokeTaskResponse {
+    /// whether the task is successfully invoked.
     #[prost(bool, required, tag="1")]
     pub success: bool,
 }
+/// Task status
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub enum TaskStatus {
+    /// If a Task is registered, the status is Initial
     Initial = 1,
+    /// If the TaskAgreement is approved by all the participants and accepted by the TMS, the status is Pending
     Pending = 2,
+    /// If the Task is scheduled and being executed in a worker, the status is Running
     Running = 3,
+    /// If the Task is executed successfully, the status is Successful
     Successful = 4,
+    /// If the Task is failed to be executed or accepted by the TMS, the status is Failed.
     Failed = 5,
 }
 #[derive(Clone, serde_derive::Serialize, serde_derive::Deserialize, Debug)]
--- mesatee_services/tms/tms_proto/src/prost_generated/tms_internal.rs
+++ mesatee_services/tms/tms_proto/src/prost_generated/tms_internal.rs
@@ -1,65 +1,94 @@
+/// Function information
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct Function {
-    #[prost(bytes, required, tag="1")]
+    /// Built-in function only needs function_id
+    #[prost(string, optional, tag="1")]
+    pub function_id: ::std::option::Option<std::string::String>,
+    /// script content
+    #[prost(bytes, required, tag="2")]
     pub script: std::vec::Vec<u8>,
 }
+/// Input Arugment
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct InputArg {
+    /// Meta information is used to verify the data integrity. 
     #[prost(message, required, tag="1")]
     pub meta: tdfs_common_proto::data_common_proto::DataMeta,
+    /// Storage information is used to access the physical data. 
     #[prost(message, required, tag="2")]
     pub storage_info: tdfs_common_proto::data_common_proto::DataStorageInfo,
+    /// Key config is used to decrypt the data.
     #[prost(message, required, tag="3")]
     pub config: kms_proto::proto::KeyConfig,
 }
+/// Output Argument
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct OutputArg {
+    /// Storage information is used to access the physical data. 
     #[prost(message, required, tag="1")]
     pub storage_info: tdfs_common_proto::data_common_proto::DataStorageInfo,
+    /// Key config is used to encrypt the data.
     #[prost(message, required, tag="2")]
     pub config: kms_proto::proto::KeyConfig,
 }
+/// ExecutableTask contains all the information the worker is required to execute the Task.
+/// Compared to TaskAgreement, data_id is replaced by meta information, physical access information and logical access information.
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct ExecutableTask {
+    /// Task identifer
     #[prost(string, required, tag="1")]
     pub task_id: std::string::String,
+    /// Task token is used for the worker to update the Task status and output meta
     #[prost(string, required, tag="2")]
     pub task_token: std::string::String,
+    /// Function information
     #[prost(message, required, tag="3")]
     pub function: Function,
+    /// Input Arguments
     #[prost(map="string, message", tag="4")]
     pub input_arguments: ::std::collections::HashMap<std::string::String, InputArg>,
+    /// Output Arguments
     #[prost(map="string, message", tag="5")]
     pub output_argments: ::std::collections::HashMap<std::string::String, OutputArg>,
 }
+/// Request for updating Task status and output meta.
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct UpdateTaskRequest {
+    /// Task identifier
     #[prost(string, required, tag="1")]
     pub task_id: std::string::String,
+    /// Task token 
     #[prost(string, required, tag="2")]
     pub task_token: std::string::String,
+    /// Task status
     #[prost(enumeration="TaskStatus", required, tag="3")]
     pub status: i32,
+    /// Output meta
     #[prost(map="string, message", tag="4")]
     pub output_meta: ::std::collections::HashMap<std::string::String, tdfs_common_proto::data_common_proto::DataMeta>,
 }
+/// Response for updating Task status and output meta.
 #[derive(Clone, PartialEq, ::prost::Message)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub struct UpdateTaskResponse {
     #[prost(bool, required, tag="1")]
     pub success: bool,
 }
+/// Task status
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, PartialOrd, Ord, ::prost::Enumeration)]
 #[repr(i32)]
 #[derive(serde_derive::Serialize, serde_derive::Deserialize)]
 pub enum TaskStatus {
+    /// If the Task is scheduled and being executed in a worker, the status is Running
     Running = 3,
+    /// If the Task is executed successfully, the status is Successful
     Successful = 4,
+    /// If the Task is failed to be executed, the status is Failed.
     Failed = 5,
 }
 #[derive(Clone, serde_derive::Serialize, serde_derive::Deserialize, Debug)]
