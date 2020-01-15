5e2f62fe1108cccedc9af076f61777df5aeb0e49
Mingshen Sun <bob@mssun.me>
2020-01-14 21:35:08 -0800

    [tests] Add protected_fs tests (untrusted part) in the test script

--- cmake/scripts/test.sh
+++ cmake/scripts/test.sh
@@ -14,10 +14,24 @@ if [ "${SGX_MODE}" = "HW" ]; then
     fi
 fi
 
+echo_title() {
+    width=70
+    padding="$(printf '%0.1s' ={1..70})"
+    padding_width="$(((width-2-${#1})/2))"
+    printf '\e[1m\e[96m%*.*s %s %*.*s\n\e[39m\e[0m' 0 "$padding_width" "$padding" "$1" 0 "$padding_width" "$padding"
+}
+
 pushd ${MESATEE_TEST_INSTALL_DIR}
 
+echo_title "encalve unit tests"
 ./teaclave_enclave_unit_test
+
+echo_title "leveldb tests"
 ./teaclave_leveldb_tests
+
+echo_title "protect_fs tests"
 ./teaclave_protected_fs_tests
+cargo test --manifest-path ${MESATEE_PROJECT_ROOT}/common/protected_fs_rs/Cargo.toml \
+      --target-dir ${MESATEE_TARGET_DIR}/untrusted
 
 popd
