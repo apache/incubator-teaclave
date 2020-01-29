f5f19fc9cfdaa1ad6c1abfba9666be17a0a28b2c
Mingshen Sun <bob@mssun.me>
2020-01-29 12:05:04 -0800

    [proto] Improve client function types and request.into() is no longer needed

--- tests/functional_tests/enclave/src/teaclave_database_service.rs
+++ tests/functional_tests/enclave/src/teaclave_database_service.rs
@@ -20,8 +20,7 @@ fn test_get_success() {
     let mut client = TeaclaveDatabaseClient::new(channel).unwrap();
     let request = GetRequest {
         key: b"test_get_key".to_vec(),
-    }
-    .into();
+    };
     let response_result = client.get(request);
     info!("{:?}", response_result);
     assert!(response_result.is_ok());
@@ -32,8 +31,7 @@ fn test_get_fail() {
     let mut client = TeaclaveDatabaseClient::new(channel).unwrap();
     let request = GetRequest {
         key: b"test_key_not_exist".to_vec(),
-    }
-    .into();
+    };
     let response_result = client.get(request);
     assert!(response_result.is_err());
 }
@@ -44,16 +42,14 @@ fn test_put_success() {
     let request = PutRequest {
         key: b"test_put_key".to_vec(),
         value: b"test_put_value".to_vec(),
-    }
-    .into();
+    };
     let response_result = client.put(request);
     info!("{:?}", response_result);
     assert!(response_result.is_ok());
 
     let request = GetRequest {
         key: b"test_put_key".to_vec(),
-    }
-    .into();
+    };
     let response_result = client.get(request);
     info!("{:?}", response_result);
     assert!(response_result.is_ok());
@@ -65,16 +61,14 @@ fn test_delete_success() {
     let mut client = TeaclaveDatabaseClient::new(channel).unwrap();
     let request = DeleteRequest {
         key: b"test_delete_key".to_vec(),
-    }
-    .into();
+    };
     let response_result = client.delete(request);
     info!("{:?}", response_result);
     assert!(response_result.is_ok());
 
     let request = GetRequest {
         key: b"test_delete_key".to_vec(),
-    }
-    .into();
+    };
     let response_result = client.get(request);
     assert!(response_result.is_err());
 }
@@ -85,8 +79,7 @@ fn test_enqueue_success() {
     let request = EnqueueRequest {
         key: b"test_enqueue_key".to_vec(),
         value: b"test_enqueue_value".to_vec(),
-    }
-    .into();
+    };
     let response_result = client.enqueue(request);
     info!("{:?}", response_result);
     assert!(response_result.is_ok());
@@ -97,35 +90,30 @@ fn test_dequeue_success() {
     let mut client = TeaclaveDatabaseClient::new(channel).unwrap();
     let request = DequeueRequest {
         key: b"test_dequeue_key".to_vec(),
-    }
-    .into();
+    };
     let response_result = client.dequeue(request);
     assert!(response_result.is_err());
     let request = EnqueueRequest {
         key: b"test_dequeue_key".to_vec(),
         value: b"1".to_vec(),
-    }
-    .into();
+    };
     let response_result = client.enqueue(request);
     assert!(response_result.is_ok());
     let request = EnqueueRequest {
         key: b"test_dequeue_key".to_vec(),
         value: b"2".to_vec(),
-    }
-    .into();
+    };
     let response_result = client.enqueue(request);
     assert!(response_result.is_ok());
     let request = DequeueRequest {
         key: b"test_dequeue_key".to_vec(),
-    }
-    .into();
+    };
     let response_result = client.dequeue(request);
     assert!(response_result.is_ok());
     assert_eq!(response_result.unwrap().value, b"1");
     let request = DequeueRequest {
         key: b"test_dequeue_key".to_vec(),
-    }
-    .into();
+    };
     let response_result = client.dequeue(request);
     assert!(response_result.is_ok());
     assert_eq!(response_result.unwrap().value, b"2");
@@ -136,29 +124,25 @@ fn test_dequeue_fail() {
     let mut client = TeaclaveDatabaseClient::new(channel).unwrap();
     let request = DequeueRequest {
         key: b"test_dequeue_key".to_vec(),
-    }
-    .into();
+    };
     let response_result = client.dequeue(request);
     assert!(response_result.is_err());
 
     let request = EnqueueRequest {
         key: b"test_dequeue_key".to_vec(),
         value: b"1".to_vec(),
-    }
-    .into();
+    };
     let response_result = client.enqueue(request);
     assert!(response_result.is_ok());
     let request = DequeueRequest {
         key: b"test_dequeue_key".to_vec(),
-    }
-    .into();
+    };
     let response_result = client.dequeue(request);
     assert!(response_result.is_ok());
     assert_eq!(response_result.unwrap().value, b"1");
     let request = DequeueRequest {
         key: b"test_dequeue_key".to_vec(),
-    }
-    .into();
+    };
     let response_result = client.dequeue(request);
     assert!(response_result.is_err());
 }
