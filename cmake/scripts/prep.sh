72ca8cedf8f5ae316b1c3250d59d5d97ab2fb012
Mingshen Sun <bob@mssun.me>
2020-01-29 11:33:40 -0800

    [build] Fix building issue introduced by prep.sh

--- cmake/scripts/prep.sh
+++ cmake/scripts/prep.sh
@@ -23,7 +23,7 @@ cp ${CMAKE_SOURCE_DIR}/config/runtime.config.toml ${MESATEE_SERVICE_INSTALL_DIR}
 cp ${CMAKE_SOURCE_DIR}/config/runtime.config.toml ${MESATEE_TEST_INSTALL_DIR}
 cp -r ${CMAKE_SOURCE_DIR}/tests/test_cases/ ${MESATEE_TEST_INSTALL_DIR}
 # temporary workaround for sharing test_cases directory between tests and services
-rm -f ${MESATEE_SERVICE_INSTALL_DIR}/test_cases
+rm -rf ${MESATEE_SERVICE_INSTALL_DIR}/test_cases
 ln -s ${MESATEE_TEST_INSTALL_DIR}/test_cases ${MESATEE_SERVICE_INSTALL_DIR}/test_cases
 cp -r ${CMAKE_SOURCE_DIR}/tests/fixtures/ ${MESATEE_SERVICE_INSTALL_DIR}
 cp -r ${CMAKE_SOURCE_DIR}/tests/fixtures/ ${MESATEE_TEST_INSTALL_DIR}
