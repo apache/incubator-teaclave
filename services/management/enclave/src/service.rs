4275aceaabb47f54b8caa3e4ddaa6b8958d6333a
Mingshen Sun <bob@mssun.me>
2020-03-22 16:39:57 -0700

    Add Apache License in header

--- services/management/enclave/src/service.rs
+++ services/management/enclave/src/service.rs
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 use crate::task::{
     assign_input_to_task, assign_output_to_task, try_update_task_to_approved_status,
     try_update_task_to_ready_status,
