426f064a4d441920e79e9c624b8413c5dc83eae5
Zhaofeng Chen <zfc@apache.org>
2020-04-06 22:51:14 -0700

    [tests] Unify functional tests requests logic.

--- tests/functional/enclave/src/end_to_end/mesapy_echo.rs
+++ tests/functional/enclave/src/end_to_end/mesapy_echo.rs
@@ -20,9 +20,10 @@ use super::*;
 pub fn test_echo_task_success() {
     // Authenticate user before talking to frontend service
     let mut api_client =
-        create_authentication_api_client(&ENCLAVE_INFO, AUTH_SERVICE_ADDR).unwrap();
+        create_authentication_api_client(shared_enclave_info(), AUTH_SERVICE_ADDR).unwrap();
     let cred = login(&mut api_client, USERNAME, PASSWORD).unwrap();
-    let mut client = create_frontend_client(&ENCLAVE_INFO, FRONTEND_SERVICE_ADDR, cred).unwrap();
+    let mut client =
+        create_frontend_client(shared_enclave_info(), FRONTEND_SERVICE_ADDR, cred).unwrap();
 
     let script = "
 def entrypoint(argv):
@@ -56,11 +57,7 @@ def entrypoint(argv):
 
     // Assign Data To Task
     let task_id = response.task_id;
-    let request = AssignDataRequest {
-        task_id: task_id.clone(),
-        input_map: HashMap::new(),
-        output_map: HashMap::new(),
-    };
+    let request = AssignDataRequest::new(task_id.clone(), hashmap!(), hashmap!());
     let response = client.assign_data(request).unwrap();
 
     log::info!("Assign data: {:?}", response);
