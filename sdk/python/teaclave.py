8085e06babca89c33f174465774adb4995945fbb
Mingshen Sun <bob@mssun.me>
2020-05-19 19:50:30 -0700

    [sdk] Improve the Python SDK and update examples (#303)

--- sdk/python/teaclave.py
+++ sdk/python/teaclave.py
@@ -5,6 +5,9 @@ import json
 import base64
 import toml
 import os
+import time
+import ssl
+import socket
 
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
@@ -14,9 +17,175 @@ from OpenSSL.crypto import X509Store, X509StoreContext
 from OpenSSL import crypto
 
 
+class RequestEncoder(json.JSONEncoder):
+    def default(self, o):
+        return o.__dict__
+
+
+class UserRegisterReqeust:
+    def __init__(self, user_id, user_password):
+        self.request = "user_register"
+        self.id = user_id
+        self.password = user_password
+
+
+class UserLoginRequest:
+    def __init__(self, user_id, user_password):
+        self.request = "user_login"
+        self.id = user_id
+        self.password = user_password
+
+
+class AuthenticationClient:
+    def __init__(self, channel):
+        self.channel = channel
+
+    def user_register(self, user_id, user_password):
+        request = UserRegisterReqeust(user_id, user_password)
+        write_message(self.channel, request)
+        return read_message(self.channel)
+
+    def user_login(self, user_id, user_password):
+        request = UserLoginRequest(user_id, user_password)
+        write_message(self.channel, request)
+        response = read_message(self.channel)
+        return response["content"]["token"]
+
+
+class AuthenticationService:
+    context = ssl._create_unverified_context()
+
+    def __init__(self, address, as_root_ca_cert_path, enclave_info_path):
+        self.address = address
+        self.as_root_ca_cert_path = as_root_ca_cert_path
+        self.enclave_info_path = enclave_info_path
+
+    def connect(self):
+        sock = socket.create_connection(self.address)
+        channel = self.context.wrap_socket(sock,
+                                           server_hostname=self.address[0])
+        cert = channel.getpeercert(binary_form=True)
+        verify_report(self.as_root_ca_cert_path, self.enclave_info_path, cert,
+                      "authentication")
+
+        return channel
+
+
+class FrontendService:
+    context = ssl._create_unverified_context()
+
+    def __init__(self, address, as_root_ca_cert_path, enclave_info_path):
+        self.address = address
+        self.as_root_ca_cert_path = as_root_ca_cert_path
+        self.enclave_info_path = enclave_info_path
+
+    def connect(self):
+        sock = socket.create_connection(self.address)
+        channel = self.context.wrap_socket(sock,
+                                           server_hostname=self.address[0])
+        cert = channel.getpeercert(binary_form=True)
+        verify_report(self.as_root_ca_cert_path, self.enclave_info_path, cert,
+                      "frontend")
+
+        return channel
+
+
+class RegisterFunctionRequest:
+    def __init__(self, metadata, name, description, executor_type, public,
+                 payload, arguments, inputs, outputs):
+        self.request = "register_function"
+        self.metadata = metadata
+        self.name = name
+        self.description = description
+        self.executor_type = executor_type
+        self.public = public
+        self.payload = payload
+        self.arguments = arguments
+        self.inputs = inputs
+        self.outputs = outputs
+
+
+class CreateTaskRequest:
+    def __init__(self, metadata, function_id, function_arguments, executor,
+                 inputs_ownership, outputs_ownership):
+        self.request = "create_task"
+        self.metadata = metadata
+        self.function_id = function_id
+        self.function_arguments = function_arguments
+        self.executor = executor
+        self.inputs_ownership = inputs_ownership
+        self.outputs_ownership = outputs_ownership
+
+
+class InvokeTaskRequest:
+    def __init__(self, metadata, task_id):
+        self.request = "invoke_task"
+        self.metadata = metadata
+        self.task_id = task_id
+
+
+class GetTaskRequest:
+    def __init__(self, metadata, task_id):
+        self.request = "get_task"
+        self.metadata = metadata
+        self.task_id = task_id
+
+
+class FrontendClient:
+    def __init__(self, channel, metadata):
+        self.channel = channel
+        self.metadata = metadata
+
+    def register_function(self,
+                          name,
+                          description,
+                          executor_type,
+                          public=True,
+                          payload=[],
+                          arguments=[],
+                          inputs=[],
+                          outputs=[]):
+        request = RegisterFunctionRequest(self.metadata, name, description,
+                                          executor_type, public, payload,
+                                          arguments, inputs, outputs)
+        write_message(self.channel, request)
+        response = read_message(self.channel)
+        return response["content"]["function_id"]
+
+    def create_task(self,
+                    function_id,
+                    function_arguments,
+                    executor,
+                    inputs_ownership=[],
+                    outputs_ownership=[]):
+        request = CreateTaskRequest(self.metadata, function_id,
+                                    function_arguments, executor,
+                                    inputs_ownership, outputs_ownership)
+        write_message(self.channel, request)
+        response = read_message(self.channel)
+        return response["content"]["task_id"]
+
+    def invoke_task(self, task_id):
+        request = InvokeTaskRequest(self.metadata, task_id)
+        write_message(self.channel, request)
+        response = read_message(self.channel)
+        assert (response["result"] == "ok")
+
+    def get_task_result(self, task_id):
+        request = GetTaskRequest(self.metadata, task_id)
+
+        while True:
+            write_message(self.channel, request)
+            response = read_message(self.channel)
+            time.sleep(1)
+            if response["content"]["status"] == 10:
+                break
+
+        return response["content"]["result"]["result"]["Ok"]["return_value"]
+
+
 def write_message(sock, message):
-    message = json.dumps(message)
-    message = message.encode()
+    message = json.dumps(message, cls=RequestEncoder).encode()
     sock.write(struct.pack(">Q", len(message)))
     sock.write(message)
 
@@ -28,7 +197,8 @@ def read_message(sock):
     return response
 
 
-def verify_report(as_root_ca_cert_path, enclave_info_path, cert, endpoint_name):
+def verify_report(as_root_ca_cert_path, enclave_info_path, cert,
+                  endpoint_name):
     if os.environ.get('SGX_MODE') == 'SW':
         return
 
@@ -58,8 +228,8 @@ def verify_report(as_root_ca_cert_path, enclave_info_path, cert, endpoint_name):
     quote = base64.b64decode(quote)
 
     # get mr_enclave and mr_signer from the quote
-    mr_enclave = quote[112:112+32].hex()
-    mr_signer = quote[176:176+32].hex()
+    mr_enclave = quote[112:112 + 32].hex()
+    mr_signer = quote[176:176 + 32].hex()
 
     # get enclave_info
     enclave_info = toml.load(enclave_info_path)
