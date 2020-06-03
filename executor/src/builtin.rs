96ffbfdca9d38bf9ab672f3bb3392073b381a2e7
Qinkun bao <qinkunbao@gamil.com>
2020-06-02 21:14:30 -0700

    add psi

--- executor/src/builtin.rs
+++ executor/src/builtin.rs
@@ -19,7 +19,8 @@
 use std::prelude::v1::*;
 
 use teaclave_function::{
-    Echo, GbdtPredict, GbdtTrain, LogisticRegressionPredict, LogisticRegressionTrain, OnlineDecrypt,
+    Echo, GbdtPredict, GbdtTrain, LogisticRegressionPredict, LogisticRegressionTrain,
+    OnlineDecrypt, PSI,
 };
 use teaclave_types::{FunctionArguments, FunctionRuntime, TeaclaveExecutor};
 
@@ -51,6 +52,8 @@ impl TeaclaveExecutor for BuiltinFunctionExecutor {
             }
             #[cfg(feature = "builtin_online_decrypt")]
             OnlineDecrypt::NAME => OnlineDecrypt::new().run(arguments, runtime),
+            #[cfg(feature = "builtin_psi")]
+            PSI::NAME => PSI::new().run(arguments, runtime),
             _ => bail!("Function not found."),
         }
     }
